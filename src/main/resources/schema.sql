d08c97babb8be2f150898be735280ae86fbc19ee
Sayan Krishna Som <31820437+sayan-som@users.noreply.github.com>
2021-06-13 10:39:33 +0530

    Update schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,3 +4,15 @@ CREATE SEQUENCE IF NOT EXISTS SEQ_CUST
     increment by 1
     start with 1001
     nocycle;
+    
+create table customer (
+ID VARCHAR(20) PRIMARY KEY,
+Name VARCHAR(20),
+country varchar(20),
+phone_number varchar(20));
+
+create table OWNER (
+CUSTOMER_ID VARCHAR(20) ,
+OWNER_Name VARCHAR(20),
+OWNER_SSN varchar(20),
+PRIMARY KEY(CUSTOMER_ID,OWNER_Name,OWNER_SSN));    
